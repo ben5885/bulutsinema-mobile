bfa6b294c3305ca18780eb0b1cbc3b0ae88b00ec
ben5885 <160017871+ben5885@users.noreply.github.com>
2026-07-17 23:39:29 +0300

    Delete bulutsinema_mobile directory

--- bulutsinema_mobile/lib/Application/search/search_event.dart
+++
@@ -1,8 +0,0 @@
-abstract class SearchEvent {}
-
-class SearchSubmitted extends SearchEvent {
-  final String term;
-  SearchSubmitted(this.term);
-}
-
-class SearchCleared extends SearchEvent {}
--- bulutsinema_mobile/lib/Core/constants.dart
+++
@@ -1,9 +0,0 @@
-class AppConstants {
-  AppConstants._();
-
-  static const String baseUrlPrefKey = 'api_base_url';
-
-  /// PC'nin api_server.py çalıştırdığı adres. Telefon ve PC aynı Wi-Fi/hotspot
-  /// ağında olmalı. Uygulama içindeki Ayarlar (⚙) ekranından değiştirilebilir.
-  static const String defaultBaseUrl = 'http://192.168.137.1:5005';
-}
